7338e5a11704d5cacc5b11657474006fa8179b9f
sidaga kihongo <sidagawazirikihongo@gmail.com>
2024-06-18 09:55:07 +0300

    Second commit

---
+++ Pie.py
@@ -0,0 +1,15 @@
+import matplotlib.pyplot as plt
+labels = ('Python', 'Java', 'Scala', 'C#')
+sizes = [45,30,15,10]
+
+#Only explode the first slice
+
+explode = (0.1,0,0,0)
+plt.pie(sizes,
+explode = explode,labels=labels,autopct='%1.f%%',
+shadow=True,
+counterclock = False,
+startangle = 45)
+plt.savefig('Pie.jpg')
+plt.show()
+
